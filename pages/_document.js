c4b0e34f6998cd62b808f7d4493dd516e5c7708e
Bradley Szoke <bradleyszoke@gmail.com>
2022-03-21 23:01:06 -0500

    minor seo fixes

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
@@ -12,6 +12,10 @@ export default function Document() {
           rel="stylesheet"
           href="https://unpkg.com/dracula-prism/dist/css/dracula-prism.css"
         ></link>
+        <meta
+          name="description"
+          content="Personal website and blog of Bradley Szoke."
+        />
       </Head>
       <body>
         <Main />
